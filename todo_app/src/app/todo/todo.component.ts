36b01be48a8be7450c651f9f7281bd2a49277665
Tat <06805645@dwc-legazpi.edu>
2022-12-02 20:38:54 +0800

    added drag&droplistGroup to todo (from angular material)

--- todo_app/src/app/todo/todo.component.ts
+++ todo_app/src/app/todo/todo.component.ts
@@ -1,5 +1,7 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { tasks } from '../task';
 
 @Component({
   selector: 'app-todo',
@@ -10,6 +12,17 @@ export class TodoComponent implements OnInit {
 
   todoForm!: FormGroup;
 
+  tasks: tasks [] = [];
+  inProgress: tasks [] = [];
+
+
+
+
+
+
+
+
+  
 
   constructor(private formB: FormBuilder) { }
 
@@ -17,6 +30,20 @@ export class TodoComponent implements OnInit {
     this.todoForm = this.formB.group({
       item: ['' ,Validators.required]
     })
+  }
+
+
+  drop(event: CdkDragDrop<tasks[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
